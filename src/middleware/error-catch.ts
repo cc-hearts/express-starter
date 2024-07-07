6b68526ef3f7174912613579e15aa0da76c76df7
Carl Chen <ccheartszzz@gmail.com>
2024-07-07 11:03:31 +0800

    fix: fix ts error

--- src/middleware/error-catch.ts
+++ src/middleware/error-catch.ts
@@ -1,8 +1,8 @@
-import type { fn } from '@cc-heart/utils/helper'
+import type { Fn } from '@cc-heart/utils/helper'
 import type { Express, Request, Response } from 'express'
 import { useHookFactory } from '../hooks/use-hook-factory.js'
 
-function errorCatch(err: Error, req: Request, res: Response, next: fn) {
+function errorCatch(err: Error, req: Request, res: Response, next: Fn) {
   const { useThrowServiceError } = useHookFactory(req, res)
   console.error('[error catch]:', err)
   useThrowServiceError(err?.toString())
